d8a0499815beac7e4630e469fc9eab626ceb54e0
Muhammad Choudhry <92322463+msc49@users.noreply.github.com>
2022-04-06 00:43:16 +0100

    Time: 44 ms (53.70%), Space: 13.9 MB (59.02%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,39 +1,5 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         
-        hash = {"a": 0,
-               "b": 0,
-               "c": 0,
-               "d": 0,
-               "e": 0,
-               "f": 0,
-               "g": 0,
-               "h": 0,
-               "i": 0,
-               "j": 0,
-               "k": 0,
-               "l": 0,
-               "m": 0,
-               "n": 0,
-               "o": 0,
-               "p": 0,
-               "q": 0,
-               "r": 0,
-               "s": 0,
-               "t": 0,
-               "u": 0,
-               "v": 0,
-               "w": 0,
-               "x": 0,
-               "y": 0,
-               "z": 0}
-        
-        for i in sentence:
-            hash[i] += 1
-        count = 0
-        for i in (hash):
-            if hash[i] > 0:
-                count += 1
-        return count == 26
-            
-        
+        a = len(set(sentence))
+        return a == 26
